7e1317e339a0ad7a8834bf98bb8907f972f131c9
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-06-14 18:23:10 +0300

    Auth user in firebase

--- app/src/main/java/com/moringaschool/e_store/RegistrationActivity.java
+++ app/src/main/java/com/moringaschool/e_store/RegistrationActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.e_store.ui.Order;
 import com.moringaschool.e_store.ui.ProductDetails;
 
@@ -18,7 +19,7 @@ import butterknife.ButterKnife;
 
 public class RegistrationActivity extends AppCompatActivity {
     @BindView(R.id.signUpButton)
-    Button SignUp;
+    Button signUser;
     @BindView(R.id.signIn_reg)
     TextView signInReg;
     @BindView(R.id.name)
@@ -28,10 +29,13 @@ public class RegistrationActivity extends AppCompatActivity {
     @BindView(R.id.password_reg)
     EditText password;
 
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        auth = FirebaseAuth.getInstance();
+
         ButterKnife.bind(this);
         signInReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +44,18 @@ public class RegistrationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        signUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+    }
+
+    private void createUser() {
+        String userName=name.getText().toString();
+        String userEmail=email.getText().toString();
+        String userPassword= password.getText().toString();
     }
 }
