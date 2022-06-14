6fb0e96dcbd660fce7b04dc2d1c460b562f48223
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-06-14 03:55:14 +0300

    Add onClickListener to login and signUp activities

--- app/src/main/java/com/moringaschool/e_store/LoginActivity.java
+++ app/src/main/java/com/moringaschool/e_store/LoginActivity.java
@@ -1,13 +1,33 @@
 package com.moringaschool.e_store;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    @BindView(R.id.signInButton)
+    Button signIn;
+    @BindView(R.id.email_login)
+    EditText email;
+    @BindView(R.id.password_login)
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/moringaschool/e_store/RegistrationActivity.java
+++ app/src/main/java/com/moringaschool/e_store/RegistrationActivity.java
@@ -2,13 +2,47 @@ package com.moringaschool.e_store;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SignalStrengthUpdateRequest;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class RegistrationActivity extends AppCompatActivity {
+    @BindView(R.id.signUpButton)
+    Button signUp;
+    @BindView(R.id.signIn)
+    TextView signIn;
+    @BindView(R.id.name)
+    EditText name;
+    @BindView(R.id.email)
+    EditText email;
+    @BindView(R.id.password)
+    EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 }
